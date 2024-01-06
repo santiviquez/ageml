47252a5cb27694152307358fe639e8e72d02df9a
Santiago Viquez <santi.viquez@gmail.com>
2024-01-07 00:33:42 +0100

    wip: basic plotting working

--- tests/test_temporal_degradation.py
+++ tests/test_temporal_degradation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 
 from ageml import TemporalDegradation
 
@@ -15,21 +16,24 @@ model = LinearRegression()
 experiment = TemporalDegradation(
     timestamp_column_name='inference_time',
     target_column_name='demand',
-    n_train_samples=30,
-    n_test_samples=20,
-    n_prod_samples=10,
-    n_simulations=3)
+    n_train_samples=52,
+    n_test_samples=12,
+    n_prod_samples=24,
+    n_simulations=100)
 
 X_train, X_test, X_prod, y_train, y_test, y_prod = experiment._train_test_prod_split(X, y)
 
-assert len(X_train) == 30, "len(X_train) should be 30"
-assert len(X_test) == 20, "len(X_test) should be 20"
-assert len(X_prod) == 10, "len(X_prod) should be 10"
+assert len(X_train) == 52, "len(X_train) should be 52"
+assert len(X_test) == 12, "len(X_test) should be 12"
+assert len(X_prod) == 24, "len(X_prod) should be 24"
 
-assert len(y_train) == 30, "len(y_train) should be 30"
-assert len(y_test) == 20, "len(y_test) should be 20"
-assert len(y_prod) == 10, "len(y_prod) should be 10"
+assert len(y_train) == 52, "len(y_train) should be 52"
+assert len(y_test) == 12, "len(y_test) should be 12"
+assert len(y_prod) == 24, "len(y_prod) should be 24"
 
-results = experiment.run(data, model)
+experiment.run(data, model)
 
+results = experiment.get_results()
 print(results)
+
+experiment.plot(metric=mean_absolute_error, plot_name='Hola')
